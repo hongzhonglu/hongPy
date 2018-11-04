a7a08576885ba7b011d0783a9b8c6c0094b97727
hongzhonglu <luho@chalmers.se>
2018-11-04 20:36:11 +0100

    feat: add the code for hongPy

---
+++ primitive/fill.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# -*- python 3 -*-
+# -*- hongzhong Lu -*-
+def add(x,y):
+    print(x+y)
---
+++ primitive/line.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# -*- python 3 -*-
+# -*- hongzhong Lu -*-
+def hello():
+    print('hello, world!')
